ee5d1f21425c2d7e00a1ec13de6b55779711a31d
hayder-alraeai <haydar860116@hotmail.com>
2021-06-17 12:57:57 +0200

    fixed cors

--- index.js
+++ index.js
@@ -8,32 +8,46 @@ const app = express()
 const server = http.createServer(app)
 const io = socketio(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "https://www.chat.alraeai.se",
       methods: ["GET", "POST"]
     }
   }) //this line is to deal with cors!!!
 io.on('connection', (socket) => {
     socket.on('join', ({name, room}, callback) => {
-        const {error, user} = addUser({id: socket.id, name, room})
-        console.log(user)
-        if(error) return callback(error)
-        //this message is made to the user from admin
-        socket.emit('message', {user: 'admin', text: `${user.name}, Welcom to the room ${user.room}`})
-        //this message is for anyone else in the chat room
-        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined!`})
-        socket.join(user.room)
-        callback()
+        try {
+            const {error, user} = addUser({id: socket.id, name, room})
+            if(error) return callback(error)
+            //this message is made to the user from admin
+            socket.emit('message', {user: 'admin', text: `${user.name}, Welcom to the room ${user.room}`})
+            //this message is for anyone else in the chat room
+            socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has joined!`})
+            socket.join(user.room)
+            //get users in the room
+            io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
+            callback()
+        }catch(e) {
+            console.log(e);
+        }
     })
 
     socket.on('sendMessage', (message, callback) => {
-        const user = getUser(socket.id)
-        io.to(user.room).emit('message', {user: user.name, text: message})
-        callback()
+        try{
+            const user = getUser(socket.id)
+            io.to(user.room).emit('message', {user: user.name, text: message})
+            io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)})
+            callback()
+        }catch(e){
+            console.log(e);
+        }
     })
 
     socket.on('disconnect', () => {
-        const user = removeUser(socket.id)
-        socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has left!`})
+        try{
+            const user = removeUser(socket.id)
+            socket.broadcast.to(user.room).emit('message', {user: 'admin', text: `${user.name}, has left!`})
+        }catch(e){
+            console.log(e);
+        }
     })
 })
 
